22cadfe08f5310614b635cdc7ad9142ded189167
bh7cw <ibh7cw@gmail.com>
2020-07-05 21:13:22 -0400

    add hw

---
+++ hw/1_PineValey_CREATE_table.sql
@@ -0,0 +1,56 @@
+CREATE DATABASE INFO6210;
+
+USE INFO6210;
+
+CREATE TABLE Customer
+(
+	CustomerID int not null,
+	CustomerName nvarchar(25),
+	CustomerAddress varchar(30),
+	CustomerCity varchar(20),
+	CustomerState char(2),
+	CustomerPostalCode varchar(9)
+CONSTRAINT Customer_PK primary key (CustomerID )
+);
+
+CREATE TABLE Product 
+(
+ProductID int not null,
+ProductDescription  varchar(50),
+ProductFinish  varchar(20)  CONSTRAINT ProductFinish_CHK CHECK ( ProductFinish IN ('Cherry',
+													 'Natural Ash', 'White Ash', 'Red Oak',
+														'Natural Oak',  'Walnut') ),
+ProductStandardPrice DECIMAL(6,2),
+ProductLineID		int,
+CONSTRAINT Product_PK PRIMARY KEY (ProductID)
+);
+
+CREATE TABLE [Order]
+(
+	OrderID int not null,
+	OrderDate Datetime default ( getdate()) ,
+	CustomerID int not null
+	CONSTRAINT Order_PK  PRIMARY KEY (OrderID),
+	CONSTRAINT  Order_FK FOREIGN KEY (CustomerID) references Customer(CustomerID) 
+	--ON UPDATE NO ACTION
+);
+
+CREATE TABLE OrderLine
+(
+	OrderID int not null,
+	ProductID int not null,
+	OrderQuantity smallint,
+	CONSTRAINT OrderLine_PK PRIMARY KEY (OrderID, ProductID),
+	CONSTRAINT OrderLine_FK1 FOREIGN KEY (OrderID) REFERENCES [Order] (OrderID),
+	CONSTRAINT  OrderLine_FK2 FOREIGN KEY( ProductID) REFERENCES Product(ProductID)
+);
+
+ 
+ -- Add new column
+ALTER TABLE Customer ADD  CustomerType varchar(2) default ( 'Commercial')  
+
+
+ 
+ -- Add new column
+ALTER TABLE Customer ALTER  column CustomerType varchar(20)  
+
